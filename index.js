1dc4f117ce98b4f7f42ab3e8d6be4c4c17b6c970
unknown <qusai.ghalil.90@gmail.com>
2023-01-10 09:35:55 +0300

    day weather and some styles added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /*declaring a function to fetch data*/
+var hoursArray = [];
 const getWeather = async (country) => {
   const response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=da68e88766cb4c29beb120051230801&q=${country}&days=3&aqi=no&alerts=no`
+    `http://api.weatherapi.com/v1/forecast.json?key=da68e88766cb4c29beb120051230801&q=${country}&days=5&aqi=no&alerts=no`
   );
 
   const result = await response.json();
@@ -16,9 +17,27 @@ const getWeather = async (country) => {
   dayWinds.textContent = `${result.current.wind_kph} kph`;
   dayWindIcon.classList.add("show");
   dayWeather.classList.add("show-shadow");
-};
+  /* mapping on results.forecast array  */
 
-// console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
+  result.forecast.forecastday.map((e) => {
+    var day = document.createElement("div");
+
+    day.classList.add("week-day");
+    day.setAttribute("data-id", e.date_epoch);
+    day.innerHTML = `
+    <p>${e.date}</p>
+    <img src="${e.day.condition.icon}"  />
+    <p>${e.day.condition.text}</p>
+    <p>Sunrise: ${e.astro.sunrise}</p>
+    <p>Sunset: ${e.astro.sunset}</p>
+    <p>AVG Temp: ${e.day.avgtemp_c} <sup>&#9900</sup> c</p>
+    `;
+    weekWeatherSection.appendChild(day);
+    /*set click event to each day */
+
+    hoursArray.push(e.hour);
+  });
+};
 
 const city = document.getElementById("city");
 const date = document.getElementById("date");
@@ -32,6 +51,8 @@ const dayWeatherIcon = document.getElementById("day-weather-icon");
 const dayWinds = document.getElementById("day-winds");
 const dayTempIcon = document.getElementById("day-temp-icon");
 const dayWindIcon = document.getElementById("day-wind-icon");
+const weekWeatherSection = document.getElementById("week-weather");
+const hoursWeatherSection = document.getElementById("hours-weather");
 /** dark mode */
 const toggle = document.getElementById("toggle");
 toggle.addEventListener("click", () => {
@@ -42,5 +63,7 @@ toggle.addEventListener("click", () => {
 });
 /* set a click event to the search icon */
 searchIocn.addEventListener("click", () => {
+  /*make sure to clean the previus results */
+  weekWeatherSection.innerHTML = "";
   getWeather(input.value);
 });
